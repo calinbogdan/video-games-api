7506122adbd7bab19b6980a42091b9c5e0e9f202
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-28 00:13:47 +0200

    Created the VideoGamesContext class

---
+++ VideoGamesApi/Data/VideoGamesContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VideoGamesApi.Models;
+
+namespace VideoGamesApi.Data
+{
+    public class VideoGamesContext : DbContext
+    {
+        public VideoGamesContext(DbContextOptions<VideoGamesContext> options) : base(options) { }
+        
+        public DbSet<VideoGame> VideoGames { get; set; }
+        public DbSet<Platform> Platforms { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Developer> Developers { get; set; }
+    }
+}
