fae75c700d22d5950873c601f637e5d5d00a8bb7
Bogdan Călin <calin.bogdan15@gmail.com>
2017-11-28 01:02:03 +0200

    Fixed the folder mistake

---
+++ VideoGamesApi/Controllers/VideoGamesController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoGamesApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/VideoGames")]
+    public class VideoGamesController : Controller
+    {
+    }
+}
